e21795734775eb91a7a041e4bdeb6afca1bb9d90
Alexandr <alexandr.lobanov.mail@mail.ru>
2017-10-13 16:09:08 +0200

    JavaScript-Task_2 (Update , Comlete):updated prime numbers script

--- JavaScript-Task_2/prime-number.js
+++ JavaScript-Task_2/prime-number.js
@@ -11,7 +11,7 @@ for(let i = 2; i < primeNumbers.length; i++)
     {
         continue;
     }
-    for (let index = 2*number; index < primeNumbers.length; index = index + index)
+    for (let index = 2*number; index < primeNumbers.length; index = index + number)
     {
         primeNumbers[index]=0;
     }
@@ -23,3 +23,26 @@ while(primeNumbers.indexOf(0)!=-1)
     primeNumbers.splice(primeNumbers.indexOf(0),1);//as you wish splice()
 }
 console.log(`Простые числа до ${maxNumber}: ${primeNumbers.join(', ')}.`);
+//-first n prime numbers-//
+var quantity=10;
+var firstPrimeNumbers=[2];
+let testedNumber=3, numberOfDeviders=0;
+while(true)
+{
+    for(let i=1;i<=testedNumber;i++)
+    {
+        numberOfDeviders +=(testedNumber % i == 0) ? 1 : 0;
+    }
+    
+    if(numberOfDeviders<=2)
+    {
+        firstPrimeNumbers.push(testedNumber);
+    }
+    if (firstPrimeNumbers.length == quantity)
+    {
+        break;
+    }
+    testedNumber++;
+    numberOfDeviders=0;
+}
+console.log(`Первые ${quantity} простых чисел: ${firstPrimeNumbers.join(', ')}.`);
